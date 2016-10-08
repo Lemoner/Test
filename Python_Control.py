f916a4190506bc6e89ffd326d731911229c4f7f5
Lemoner <401103554@qq.com>
2016-10-08 22:24:04 +0800

    Add files via upload

---
+++ Python_Control.py
@@ -0,0 +1,20 @@
+i=1
+sum=0
+while i<=100 :
+    sum+=i
+    i+=1
+print(sum)
+
+for i in range(1,5):
+    print(i)
+if 0 :
+    print('hrll')
+
+for i in range(1,10):
+    if i>6:
+        continue
+    print(i,"hell")
+#         break
+
+#     else:
+#         print(i)
